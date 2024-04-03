f499383b300d09e9372bd16704673b093a0c33a5
Iris Liang <l2620090@gmail.com>
2024-04-04 03:09:17 +1100

    Finish on click function

--- src/pages/FirstPage/FirstPage.jsx
+++ src/pages/FirstPage/FirstPage.jsx
@@ -8,6 +8,7 @@ import kntHeart5 from "../../assests/img/sceneOne/knt5.png";
 import kntHeart4 from "../../assests/img/sceneOne/knt4.png";
 import kntHeart3 from "../../assests/img/sceneOne/kntheart3.png";
 import kntHeart2 from "../../assests/img/sceneOne/kntheart2.png";
+import kntHeart1 from "../../assests/img/sceneOne/kntheart0.png";
 import kntHeart0 from "../../assests/img/sceneOne/kntheart0.png";
 import nytHeart5 from "../../assests/img/sceneOne/nyt5.png";
 import kntOne from "../../assests/img/sceneOne/knt1.png";
@@ -18,6 +19,15 @@ import BGAudioPlayer from "../../utils/BGAudioPlayer";
 import startBG from "../../assests/img/sceneOne/StartScene.png";
 import startButton from "../../assests/img/sceneOne/StartButton.png";
 import closeButton from "../../assests/img/sceneOne/CloseButton.png";
+import catMeow1 from "../../assests/audio/cat-meow-1.mp3";
+import catMeow2 from "../../assests/audio/cat-meow-2.mp3";
+import catMeow3 from "../../assests/audio/cat-meow-3.mp3";
+import angryCatMeow1 from "../../assests/audio/angry-cat.mp3";
+import angryCatMeow2 from "../../assests/audio/very-angry-cat.mp3";
+import angry1 from "../../assests/img/sceneOne/kntAngery1.png";
+import angry2 from "../../assests/img/sceneOne/kntAngery2.png";
+import angry3 from "../../assests/img/sceneOne/kntAngery3.png";
+import question from "../../assests/img/sceneOne/questionMark.png";
 
 const OuterContainer = styled.div`
   width: 1300px;
@@ -86,6 +96,42 @@ const CatOne = styled.img`
   }
 `;
 
+const Angry1 = styled.img`
+  position: absolute;
+  top: 200px;
+  left: 200px;
+  width: 50px;
+  height: 50px;
+  object-fit: cover;
+`;
+
+const Angry2 = styled.img`
+  position: absolute;
+  top: 150px;
+  left: 400px;
+  width: 70px;
+  height: 70px;
+  object-fit: cover;
+`;
+
+const Angry3 = styled.img`
+  position: absolute;
+  top: 130px;
+  left: 260px;
+  width: 90px;
+  height: 90px;
+  object-fit: cover;
+`;
+
+const QuestionMark = styled.img`
+  position: absolute;
+  top: 180px;
+  left: 650px;
+  width: auto;
+  height: auto;
+  object-fit: cover;
+`;
+
 const CatTwo = styled.img`
   position: absolute;
   top: 160px;
@@ -112,6 +158,7 @@ const CatOneHeart = styled.img`
   height: 18px;
   object-fit: cover;
 `;
+
 const CatTwoHeart = styled.img`
   position: absolute;
   top: 187px;
@@ -168,16 +215,50 @@ const FirstPage = () => {
   //Handle click KNT
   const handleClickCatOne = () => {
     setCatOneMood(catOneMood - 1);
+    if (catOneMood <= 2) {
+      new Audio(angryCatMeow2).play();
+    } else if (catTwoMood <= 3) {
+      new Audio(angryCatMeow1).play();
+    } else if (catTwoMood <= 5) {
+      new Audio(catMeow1).play();
+    } else {
+      new Audio(catMeow3).play();
+    }
   };
 
   //Handle click NYT
   const handleClickCatTwo = () => {
     setcatTwoMood(catTwoMood - 0.3);
-    console.log("click");
+    if (catTwoMood <= 2) {
+      new Audio(angryCatMeow2).play();
+    } else if (catTwoMood <= 3) {
+      new Audio(catMeow1).play();
+    } else if (catTwoMood <= 4) {
+      new Audio(catMeow2).play();
+    } else {
+      new Audio(catMeow3).play();
+    }
   };
 
   const handleClickStart = () => {
     setStart(true);
+  };
+
+  const getKNTHeartImage = (mood) => {
+    switch (Math.floor(mood)) {
+      case 5:
+        return kntHeart5;
+      case 4:
+        return kntHeart4;
+      case 3:
+        return kntHeart3;
+      case 2:
+        return kntHeart2;
+      case 1:
+        return kntHeart1;
+      default:
+        return kntHeart0;
+    }
   };
 
   return (
@@ -191,15 +272,19 @@ const FirstPage = () => {
               <CatTwo src={nytOne} alt="catTwo" onClick={handleClickCatTwo} />
               <CatOne src={kntOne} alt="catOne" onClick={handleClickCatOne} />
               <CatOneHeart
-                src={kntHeart5}
+                src={getKNTHeartImage(catOneMood)}
                 alt="catOneHeart"
                 mood={catOneMood}
               />
               <CatTwoHeart
-                src={nytHeart5}
+                src={getKNTHeartImage(catTwoMood)}
                 alt="catOneHeart"
                 mood={catTwoMood}
               />
+              {/* <Angry1 src={angry1} alt="Angry1" />
+              <Angry2 src={angry2} alt="Angry2" />
+              <Angry3 src={angry2} alt="Angry3" />
+              <QuestionMark src={question} alt="question" /> */}
             </>
           ) : (
             <div>
